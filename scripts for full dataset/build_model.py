5246db14947051857615b3af7030ab7d77cfe731
Aleksandra Astakhova <lexie@live.ru>
2019-06-13 12:36:43 +0300

    Added scripts for building a model capable to perform classification on the full dataset: 1. load_data.py script to load the data ans save preprocessed data as npz files. Parameters: --save_dir, --num_classes, --num_examples. 2. build_model.py script to train the model. 3. categories.txt file with available drawing categories.

---
+++ scripts for full dataset/build_model.py
@@ -0,0 +1,217 @@
+# import linear algebra and data manipulation libraries
+import numpy as np
+import pandas as pd
+
+# import PIL for image manipulation
+from PIL import Image, ImageDraw, ImageOps
+
+# import matplotlib for plotting
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+
+# import helper libraries
+from collections import OrderedDict
+import os
+from os import path
+import time
+import argparse
+
+# import pytorch
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from torch.utils.data import Dataset, DataLoader
+
+# import machine learning libraries
+from sklearn.model_selection import train_test_split
+
+# import utilities to load the dataset
+from load_data import load_categories, load_data
+
+def convert_to_PIL(img, width = 256, height = 256):
+    """
+    Function to convert numpy (width, height) image to PIL image.
+    INPUT:
+        img - (numpy array) image from train dataset with size (width, height)
+    OUTPUT:
+        pil_img - (PIL Image) (width x height) image
+    """
+    img_r = img.reshape(width, height)
+
+    pil_img = Image.new('RGB', (width, height), 'white')
+    pixels = pil_img.load()
+
+    for i in range(0, width):
+        for j in range(0, height):
+            if img_r[i, j] > 0:
+                pixels[j, i] = (255 - int(img_r[i, j] * 255), 255 - int(img_r[i, j] * 255), 255 - int(img_r[i, j] * 255))
+
+    return pil_img
+
+class DoodlesDataset(Dataset):
+    """
+    Dataset class for doodles.
+    """
+
+    def __init__(self, data, labels, width = 256, height = 256, transform=None):
+        """
+        The initialization method.
+        INPUT:
+            1. data - images data
+            2. labels - labels data
+            3. width - image width and height
+            4. transform (callable, optional) - Optional transform to be applied
+                on a sample
+        """
+        self.data = data
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image = convert_to_PIL(self.data[idx])
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return [image, label]
+
+def initialize_model(num_categories):
+    """
+    Function returns machine learning model to be trained.
+    INPUT:
+        1. num_categories - number of classes
+    OUTPUT:
+        1. model - resulting model to be trained
+    """
+    model = models.densenet169(pretrained=False)
+    input_size = 1664
+    hidden_sizes = [512, 128, 64]
+    model.classifier = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(input_size, hidden_sizes[0])),
+                          ('relu1', nn.ReLU()),
+                          ('fc2', nn.Linear(hidden_sizes[0], hidden_sizes[1])),
+                          ('bn2', nn.BatchNorm1d(num_features=hidden_sizes[1])),
+                          ('relu2', nn.ReLU()),
+                          ('dropout', nn.Dropout(0.3)),
+                          ('fc3', nn.Linear(hidden_sizes[1], hidden_sizes[2])),
+                          ('bn3', nn.BatchNorm1d(num_features=hidden_sizes[2])),
+                          ('relu3', nn.ReLU()),
+                          ('logits', nn.Linear(hidden_sizes[2], num_categories))]))
+
+    return model
+
+def train_model(model, trainloader, testloader, epochs, batch_size, learning_rate, device):
+    '''
+    Function to train the model.
+    INPUT:
+        1. model - model to train
+        2. trainloader - data loader for training data
+        3. testloader - data loader for test data
+        4. epochs - number of epochs
+        5. batch_size - size of the batch
+        6. learning_rate - learning rate
+        7. device - cuda or gpu
+    '''
+    # set loss function
+    criterion = nn.NLLLoss()
+
+    # set optimizer, only train the classifier parameters, feature parameters are frozen
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # train the model
+    model.to(device)
+
+    steps = 0
+    running_loss = 0
+    print_every = 5
+    for epoch in range(epochs):
+        for inputs, labels in trainloader:
+            steps += 1
+            # Move input and label tensors to the default device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            logps = model.forward(inputs)
+            loss = criterion(logps, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if steps % print_every == 0:
+                test_loss = 0
+                accuracy = 0
+                roc_auc = 0
+
+                model.eval()
+                with torch.no_grad():
+                    for inputs, labels in testloader:
+                        inputs, labels = inputs.to(device), labels.to(device)
+                        logps = model.forward(inputs)
+                        batch_loss = criterion(logps, labels)
+
+                        test_loss += batch_loss.item()
+
+                        # Calculate accuracy and roc_auc
+                        ps = torch.exp(logps)
+                        top_p, top_class = ps.topk(1, dim=1)
+
+                print(f"Epoch {epoch+1}/{epochs}.. "
+                      f"Train loss: {running_loss/print_every:.3f}.. "
+                      f"Test loss: {test_loss/len(testloader):.3f}.. "
+                      f"Test accuracy: {accuracy/len(testloader):.3f}.. ")
+                running_loss = 0
+                model.train()
+
+def main():
+    # load categories and labels dictionary
+    categories, labels_dict = load_categories(num_categories = 10)
+
+    # load from pickle
+    data, labels = load_data(data_filepath = './data')
+
+    # define train transformations
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.5, 0.5, 0.5],
+                                                                [0.5, 0.5, 0.5])])
+    # define test transformations
+    test_transforms = transforms.Compose([transforms.ToTensor(),
+                                          transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+    # Split each dataset into train/test splits
+    X_train, X_test, y_train, y_test = train_test_split(data,labels,test_size=0.1,random_state=42)
+
+    # setup datasets and transformations
+    train_dataset = DoodlesDataset(X_train, y_train, transform=train_transforms)
+    test_dataset = DoodlesDataset(X_test, y_test, transform=test_transforms)
+
+    # setup dataloaders
+    trainloader = DataLoader(train_dataset, batch_size=128,
+                        shuffle=True, num_workers=0)
+
+    testloader = DataLoader(test_dataset, batch_size=32,
+                            shuffle=True, num_workers=0)
+
+    # setup Hyperparameters
+    epochs = 1
+    batch_size = 128
+    learning_rate = 0.001
+    device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
+
+    # setup the model
+    model = initialize_model(10)
+
+    # train the model
+    train_model(model, trainloader, testloader, epochs, batch_size, learning_rate, device)
+
+if __name__ == '__main__':
+    main()
---
+++ scripts for full dataset/load_data.py
@@ -0,0 +1,277 @@
+# import linear algebra and data manipulation libraries
+import numpy as np
+import pandas as pd
+
+# import helper libraries
+import requests
+from io import BytesIO # Use When expecting bytes-like objects
+import pickle
+from collections import OrderedDict
+import os
+from os import path
+import time
+import argparse
+import wget
+import ast
+
+# import PIL for image manipulation
+from PIL import Image, ImageDraw, ImageOps
+
+# ndjson to open files
+import ndjson
+
+# import matplotlib for plotting
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+
+def load_categories(num_categories = 345, filepath = 'categories.txt'):
+    '''
+    Function loads the names of drawings categories from file.
+    INPUT:
+        1. num_categories - number of categories,
+        2. filepath - fil, from which category names should be loaded.
+
+    OUTPUT:
+        1. categories - list of names of categories
+        2. label_dict - dictionary with classes names and labels
+    '''
+    # read categories.txt and create a list of categories
+    categories = []
+    with open(filepath, 'r') as f:
+        for i in range(0, num_categories):
+            categories.append(f.readline()[:-1])
+
+    # create a dictionary with labels for each category
+    label_dict = {key:categories[key] for key in range(0,num_categories)}
+
+    return categories, label_dict
+
+def load_simplified_data(categories, data_filepath = './data'):
+    '''
+    Function loads simplified ndjson files for specified categories.
+    INPUT:
+        1. categories - list of names of categories to be downloaded
+        2. data_filepath - folder name to store downloaded data
+    '''
+
+    # create a folder to store data if required
+    if not os.path.exists(data_filepath):
+        os.mkdir(data_filepath)
+
+    # download files if required
+    for category in categories: # check that file don't exist on the disk
+        if not os.path.exists(data_filepath + '/' + str(category) + '.ndjson'):
+            print("\nDownloading data for {}.".format(category))
+            url = 'https://storage.googleapis.com/quickdraw_dataset/full/simplified/' + str(category) + '.ndjson'
+            wget.download(
+                        url=url,
+                        out=data_filepath
+                    )
+        else:
+            print("Data for {} is already downloaded.".format(category))
+
+def convert_to_PIL(drawing, width = 256, height = 256):
+    """
+    Function to convert from drawing to PIL image.
+    INPUT:
+        drawing - drawing from 'drawing' column
+        width - width of the initial image
+        height - height of the initial image
+    OUTPUT:
+        pil_img - (PIL Image) image
+    """
+
+    # initialize empty (white) PIL image
+    pil_img = Image.new('RGB', (width, height), 'white')
+    pixels = pil_img.load()
+
+    draw = ImageDraw.Draw(pil_img)
+
+    # draw strokes as lines
+    for x,y in drawing:
+        for i in range(1, len(x)):
+            draw.line((x[i-1], y[i-1], x[i], y[i]), fill=0)
+
+    return pil_img
+
+def convert_to_np_raw(drawing, width = 256, height = 256):
+    """
+    INPUT:
+        drawing - drawing in initial format
+        width - width of the initial image
+        height - height of the initial image
+    OUTPUT:
+        img - drawing converted to the numpy array (width X height)
+    """
+    # initialize empty numpy array
+    img = np.zeros((width, height))
+
+    # create a PIL image out of drawing
+    pil_img = convert_to_PIL(drawing)
+
+    #resize to 28,28
+    pil_img.thumbnail((width, height), Image.ANTIALIAS)
+
+    pil_img = pil_img.convert('RGB')
+    pixels = pil_img.load()
+
+    # fill in numpy array with pixel values
+    for i in range(0, width):
+        for j in range(0, height):
+            img[i, j] = 1 - pixels[j, i][0] / 255
+
+    return img
+
+
+def convert_to_np(pil_img, width = 256, height = 256):
+    """
+    Function to convert PIL Image to numpy array.
+    INPUT:
+        pil_img - (PIL Image) image to be converted
+    OUTPUT:
+        img - (numpy array) converted image with shape (width, height)
+    """
+    pil_img = pil_img.convert('RGB')
+
+    img = np.zeros((width, height))
+    pixels = pil_img.load()
+
+    for i in range(0, width):
+        for j in range(0, height):
+            img[i, j] = 1 - pixels[j, i][0] / 255
+
+    return img
+
+def view_image(img, width = 256, height = 256, category = ''):
+    """
+    Function to view numpy image with matplotlib.
+    The function saves the image as png.
+    INPUT:
+        img - (numpy array) image from train dataset with size (1, 784)
+    OUTPUT:
+        None
+    """
+    fig, ax = plt.subplots(figsize=(6,9))
+    ax.imshow(img.reshape(width, height).squeeze())
+    ax.axis('off')
+
+    ts = time.time()
+    plt.savefig('image_' +str(category) + '_' + str(ts) + '.png')
+
+def get_drawings(category, data_filepath = './data', num_drawings = 0, width = 256, height = 256):
+    '''
+    Function to get drawing data out of ndjson file.
+    INPUT:
+        1. category - name of category to get data for
+        2. data_filepath - path to folder containing ndjson files
+        3. num_drawings - maximum number of drawings to get data for, all
+        drawings are processed by default (num_drawings = 0)
+        3. width, height - width and height of each image
+
+    OUTPUT:
+        1. np_images  - numpy images of specified category
+    '''
+    print("Preprocessing data for {}.\n".format(category))
+
+    # open ndjson file containing image data
+    filename = data_filepath + '/' + category + '.ndjson'
+    with open(filename) as f:
+        data = ndjson.load(f)
+
+    # setup the number of images to fetch from file
+    if num_drawings == 0:
+        num_drawings = len(data)
+
+    # convert images from raw to numpy
+    np_images = []
+    for i in range(min(len(data), num_drawings)):
+        img = convert_to_np_raw([ast.literal_eval(str(img)) for img in data[i]['drawing']], width = width, height = height)
+        np_images.append(img)
+
+    # return numpy images array
+    return np.array(np_images)
+
+def prepare_data(label_dict, data_filepath = './data', num_drawings = 0, width = 256, height = 256):
+    """
+    Function to get data for specified categories from the ndjson files
+    and save it as preprocessed pickle file.
+    INPUT:
+        1. categories - name of categories to get data for
+        2. data_filepath - path to folder containing ndjson files
+        3. num_drawings - maximum number of drawings to get data for each
+        category, all drawings are processed by default (num_drawings = 0)
+        3. width, height - width and height of each image
+
+    OUTPUT:
+        None, the data is saved as npz
+    """
+    data = []
+    labels = []
+    # enumerate categories
+    for key, value in label_dict.items():
+        # get data for each category
+        cat_data = get_drawings(value, data_filepath = data_filepath, num_drawings = num_drawings, width = width, height=height)
+        [data.append(np_img) for np_img in cat_data]
+        labels.append([key for i in range(num_drawings)])
+
+    # convert to numpy array
+    data = np.array(data)
+    labels = np.array(labels)
+    labels = labels.reshape(labels.shape[0] * labels.shape[1])
+
+    # save data as npz
+    np.savez(data_filepath + '/' + 'data.npz', data)
+    np.savez(data_filepath + '/' + 'labels.npz', labels)
+
+def load_data(data_filepath = './data'):
+    """
+    Function loads data from pickle files.
+    INPUT:
+        1. data_filepath - path to stored preprocessed data
+    OUTPUT:
+        2. data, labels - loaded image data and labels
+    """
+    data = np.load(data_filepath + '/' + 'data.npz')['arr_0']
+    labels = np.load(data_filepath + '/' + 'labels.npz')['arr_0']
+
+    return data, labels
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Argument parser')
+
+    parser.add_argument('--save_dir', action='store', default = './data',
+                        help='Directory to save preprocessed data.')
+
+    parser.add_argument('--num_classes', type = int, action='store', default = 10,
+                        help='Number of classes to retreive.')
+
+    parser.add_argument('--num_examples', type = int, action='store', default = 1000,
+                        help='Number of training examples per class.')
+
+    results = parser.parse_args()
+
+    num_categories = results.num_classes
+    num_examples = results.num_examples
+    data_filepath = results.save_dir
+
+    # load categories and create label dictionary
+    categories, label_dict = load_categories(num_categories = num_categories)
+
+    # load ndjson simplified files
+    load_simplified_data(categories, data_filepath = data_filepath)
+
+    # get images for apples
+    apples = get_drawings('apple', num_drawings = 5)
+
+    # view image example
+    view_image(apples[3], width = 256, height = 256, category = 'apple')
+
+    # prepare data and save to pickle files
+    prepare_data(label_dict, data_filepath = data_filepath, num_drawings = num_examples, width = 256, height = 256)
+
+    # load from pickle
+    data, labels = load_data(data_filepath = data_filepath)
+
+if __name__ == '__main__':
+    main()
